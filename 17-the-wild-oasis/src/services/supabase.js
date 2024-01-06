6384cc7e2b1f9cec564efe72a51e8ecfb834b598
nymuelis <daniel.tsv.georgiev@gmail.com>
2024-01-06 10:34:25 +0200

    Added add a cabin functionality

--- 17-the-wild-oasis/src/services/supabase.js
+++ 17-the-wild-oasis/src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://uzvhaytclehslkrlcxwy.supabase.co'
+export const supabaseUrl = 'https://uzvhaytclehslkrlcxwy.supabase.co'
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV6dmhheXRjbGVoc2xrcmxjeHd5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQ1MTUwOTMsImV4cCI6MjAyMDA5MTA5M30.pMxYmAOb3RPVnh7WuyAcN7RgDw_bnJS0WxfdQhlCMqw";
 const supabase = createClient(supabaseUrl, supabaseKey)
 
