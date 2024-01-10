cdadc9e696c3d9996e963fde5b872400c9dbf2b6
nymuelis <daniel.tsv.georgiev@gmail.com>
2024-01-10 11:00:30 +0200

    Final touches

--- 17-the-wild-oasis/src/features/authentication/LoginForm.jsx
+++ 17-the-wild-oasis/src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("jonas@example.com");
-  const [password, setPassword] = useState("pass0987");
+  const [email, setEmail] = useState("test@test.com");
+  const [password, setPassword] = useState("testtest");
   const { login, isLoading } = useLogin();
 
   function handleSubmit(e) {
